53a52fe9b951c0171b3a89b4c78a891f12d1b946
Natalia <atvnataliajn@gmail.com>
2022-03-14 12:28:36 +0100

    add loader component

--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -1,10 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../components/Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../components/Header";
 
-test('render title in header', () => {
+test("render title in header", () => {
   render(<Header />);
-
   const title = screen.getByText(/Cool pics/i);
   expect(title).toBeTruthy();
- 
 });
