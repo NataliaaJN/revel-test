1b7c6aa2f5e43ac1de7568fe218b322314b4b149
Natalia <atvnataliajn@gmail.com>
2022-03-14 10:31:42 +0100

    add hook to get api data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
+// services
+import getApiData from "../services/api";
+// components
 import Header from "./Header";
 import PhotosList from "./PhotosList";
+// styles
 import "../styles/App.scss";
 
 const App = () => {
+  //     STATES       //
   const [photos, setPhotos] = useState([]);
 
+  //    USE EFFECTS     //
   // Take api data
-  
+  useEffect(() => {
+    getApiData()
+      .then((data) => {
+        setPhotos(data);
+      })
+  }, []);
+
+  //    FUNCTIONS      //
 
   return (
     <div className="App">
