3e9b2710c4e6ebf7c46173104ea15dc6c8c6e9aa
Natalia <atvnataliajn@gmail.com>
2022-03-18 18:42:05 +0100

    change browserRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router} from "react-router-dom";
-import { HashRouter } from 'react-router-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { HashRouter } from 'react-router-dom';
+import App from "./components/App";
+
+
 
 ReactDOM.render(
-  <HashRouter >
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </HashRouter>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
