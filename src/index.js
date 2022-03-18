70b692d29e857ec455494265754d16fefbf04114
Natalia <atvnataliajn@gmail.com>
2022-03-18 19:41:51 +0100

    update index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./components/App";
 
 
 ReactDOM.render(
-  <Router >
+  <Router basename={process.env.PUBLIC_URL}>
     {console.log("Process enviroment", process.env)}
     <App />
   </Router>,
