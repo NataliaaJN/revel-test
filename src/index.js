859f6c2dc0fff1f204942ff0c9feb1620e2beb69
Natalia <atvnataliajn@gmail.com>
2022-03-19 02:41:42 +0100

    update router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from "./components/App";
 
 
 
 ReactDOM.render(
-  <Router>
-    {console.log("Process enviroment", process.env)}
+  // <Router basename={process.env.PUBLIC_URL}>
+  <HashRouter>
+    {console.log("Process enviroment", process.env.PUBLIC_URL)}
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById("root")
 );
