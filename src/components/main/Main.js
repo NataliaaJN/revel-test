5cd991b231de288ac18c19c1c96424835c2d301a
Natalia <atvnataliajn@gmail.com>
2022-03-19 11:12:10 +0100

    fix 404 not found page error in development

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,12 +1,10 @@
 import PropTypes from "prop-types";
-import ErrorMessage from "../handleErrors/ErrorMessage";
 import Loader from "../loader/Loader";
 import PhotosList from "./PhotosList";
 
-const Main = ({ errorMessage, isLoading, photos }) => {
+const Main = ({ isLoading, photos }) => {
   return (
     <main className="main">
-      {errorMessage && <ErrorMessage errorMessage={errorMessage} />}
       <Loader isLoading={isLoading} />
       <PhotosList photos={photos} />
     </main>
@@ -14,9 +12,8 @@ const Main = ({ errorMessage, isLoading, photos }) => {
 };
 
 Main.propTypes = {
-    errorMessage: PropTypes.string,
-    isLoading: PropTypes.bool.isRequired,
-    photos: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  photos: PropTypes.array.isRequired,
 };
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { HashRouter } from 'react-router-dom';
 import App from "./components/App";
 
 
 
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
-  {/* // <Router> */}
-    {console.log("Process enviroment", process.env.PUBLIC_URL)}
     <App />
   </Router>,
   document.getElementById("root")
